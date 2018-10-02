7cd6b7de65a98d46912b96fe3a6c6f52b316b752
bam <user@es302n22>
2018-10-02 10:46:33 +0800

    varibles

--- myproject/src/com/tom/Hello.java
+++ myproject/src/com/tom/Hello.java
@@ -5,6 +5,23 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world");
+		int age = 18 ;
+		int schoolYear = 107;
+		int birthday = 727;
+		int loveNumber = 10;
+		float weight = 60.5F;
+		float height = 1.7f;
+		String name = "Bam";
+		System.out.println(age);
+		System.out.println(schoolYear);
+		System.out.println(birthday);
+		System.out.println(loveNumber);
+		System.out.println(weight);
+		System.out.println(height);
+		System.out.println(name);
+		
+		
+				
 
 	}
 
