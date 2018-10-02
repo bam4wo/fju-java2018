a5e4be869ef1fb130a1cfc5711c8ea696627c2c6
user <user@es302n22>
2018-10-02 11:13:25 +0800

    initail

--- exam/src/com/fju/Hello.java
+++ exam/src/com/fju/Hello.java
@@ -1,19 +1,12 @@
 package com.fju;
 
 public class Hello {
-	
-		public static void main(String[]args)
-		{
-			System.out.println("Hello");
-			int age = 17;
-			int number = 21;
-			float weight = 60.5f;
-			float height = 1.7f;
-			System.out.println(age);
-			System.out.println(number);
-			System.out.println(weight);
-			System.out.println(height);
-			
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+		
+ 
 	}
 
 }
