87d25bbd1f7b09e402081f6bb719ed9044d822d1
user <user@es302n22>
2018-10-23 11:59:18 +0800

    2

--- myproject/src/com/tom/RangeTester.java
+++ myproject/src/com/tom/RangeTester.java
@@ -5,10 +5,10 @@ import java.util.Scanner;
 public class RangeTester {
 
 	public static void main(String[] args) {
-		System.out.println("Import one int number(3~5)");
+		System.out.println("Import one int number(-3~5)");
 		Scanner scanner = new Scanner(System.in);
-		String line = scanner.nextLine();
-		boolean number = line.equals("3")||line.equals("4")||line.equals("5");
+		int line = scanner.nextInt();
+		boolean number = line>=-3 && line<=5;
 		System.out.println(number);
 		
 	}
--- myproject/src/com/tom/SingUp.java
+++ myproject/src/com/tom/SingUp.java
@@ -11,6 +11,22 @@ public class SingUp {
 	      //boolean adult = line.equals("y") || line.equals("Y");
 	      boolean adult = line.equalsIgnoreCase("y");
 	      System.out.println(adult);
+	      if(adult){
+	    	  System.out.print("Your age?");
+	    	  int age = scanner.nextInt();
+	    	  scanner.nextLine();
+	    	  System.out.print("Your name?");
+	    	  String name = scanner.nextLine();
+	    	  System.out.print("Your nickName?");
+	    	  String nickName = scanner.nextLine();
+	    			  
+	    	  
+	      }else{
+	    	  System.out.print("bye");
+	    	  
+	      }
+	      
+	    
 
 	}
 
