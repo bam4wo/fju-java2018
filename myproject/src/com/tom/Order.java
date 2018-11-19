dc7711adbf02561c92b9f36cc5d506d90be69f49
Chiu <Chiu@192.168.0.8>
2018-11-19 22:39:04 +0800

    11/19

--- myproject/src/com/tom/Order.java
+++ myproject/src/com/tom/Order.java
@@ -5,13 +5,26 @@ import java.util.Scanner;
 public class Order {
 	
 	public static void main(String[] args) {
-		Scanner scanner  = new Scanner(System.in);
-		System.out.println("Please enter number of tickets:");
-		int total = scanner.nextInt();
-		System.out.println("How many round-trip tickets:");
-		int rticket = scanner.nextInt();
-		System.out.println("Total:");
-		System.out.println("");
+		Scanner scanner = new Scanner(System.in);
+		int all = 1;
+		while(all != 0) {
+			System.out.print("Please enter number of tickets: ");
+			int total = scanner.nextInt();
+			scanner.nextLine();
+			if(total != -1) {
+				System.out.print("How many round-trip tickets: ");
+				int rticket = scanner.nextInt();
+				Ticket r = new Ticket(rticket);
+				Ticket t = new Ticket(total - rticket);
+				System.out.println("Total tickets:" + total);
+				System.out.println("Round-trip:" + rticket);
+				System.out.println("Total:" + (t.onePrice()+r.rPrice()));
+			}else {
+				break;
+			}
+		}
+		
+		
 	}
 
 }
--- myproject/src/com/tom/Ticket.java
+++ myproject/src/com/tom/Ticket.java
@@ -1,8 +1,22 @@
 package com.tom;
 
 public class Ticket {
-	int total;
-	int rticket;
+	int price = 1000;
+	int ticket;
+	
+	public Ticket(int ticket) {
+		this.ticket = ticket;
+	}
+	
+	public int onePrice() {
+		int onePrice = ticket*price;
+		return onePrice;
+	}
+	
+	public int rPrice() {
+		int rPrice = (int)(ticket*price*2*0.9);
+		return rPrice;
+	}
 	
 
 }
